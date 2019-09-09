9e63ec56d3d05aecd442e9da0f4e0ad926bc4b51
lideo <1573567+lideo@users.noreply.github.com>
2019-09-09 13:34:19 +0800

    Add more log methods to my custom console

--- src/js/index.js
+++ src/js/index.js
@@ -4,21 +4,6 @@ import createConsole from "./playgroundConsole";
 
 import '../sass/main.scss';
 
-const playgroundEditor = createEditor(document.getElementById('code'), executeCode);
-const playgroundConsole = createConsole();
-
-// TODO: rewrite all the rest of the console methods
-(function() {
-  const proxiedLog = console.log;
-  console.log = function(message) {
-    playgroundConsole.writeToConsole(message);
-    return proxiedLog.apply(this, arguments);
-  };
-
-  const proxiedError = console.error;
-  console.error = function(message) {
-    playgroundConsole.writeToConsole(message);
-    return proxiedError.apply(this, arguments);
-  }
-})();
+createEditor(document.getElementById('code'), executeCode);
+createConsole();
 
--- src/js/playgroundConsole.js
+++ src/js/playgroundConsole.js
@@ -5,17 +5,72 @@ const createConsole = function() {
     el.scrollTop = el.scrollHeight;
   }
 
-  const clear = () => {
-    consoleEl.innerHTML = '';
+  const messageTypeLabels = {
+    log: '',
+    info: 'Info',
+    warn: 'Warning',
+    error: 'Error',
   }
 
-  const write = message => {
+  function stringifyArguments(list) {
+    return list.reduce(function(accumulator, current) {
+      return `${accumulator} ${JSON.stringify(current, null, ' ')}`;
+    }, '');
+  }
+
+  const write = (level = 'log', consoleArguments) => {
     const node = document.createElement('div');
-    node.className = 'console-line';
-    node.innerHTML = `<p><span>></span> ${message}</p>`;
+    node.className = `console-line ${level}`;
+
+    const message = stringifyArguments([...consoleArguments]);
+
+    let line = '<p><span>></span>';
+    line += (level != 'log') ? `<span class="level">${messageTypeLabels[level]}</span> ` : '';
+    line += `${message}</p>`;
+
+    node.innerHTML = line;
     consoleEl.appendChild(node);
     scrollToBottom(consoleEl);
   }
+
+  const clear = () => {
+    consoleEl.innerHTML = '';
+    write('log', 'Console was cleared.');
+  }
+
+  // TODO: rewrite all the rest of the console methods
+  // and maybe refactor...
+  (function() {
+    const proxiedLog = console.log;
+    console.log = function() {
+      write('log', arguments);
+      return proxiedLog.apply(console, arguments);
+    };
+
+    const proxiedError = console.error;
+    console.error = function() {
+      write('error', arguments);
+      return proxiedError.apply(console, arguments);
+    }
+
+    const proxiedInfo = console.info;
+    console.info = function() {
+      write('info', arguments);
+      return proxiedInfo.apply(console, arguments);
+    };
+
+    const proxiedWarn = console.warn;
+    console.warn = function() {
+      write('warn', arguments);
+      return proxiedWarn.apply(console, arguments);
+    };
+
+    const proxiedClear = console.clear;
+    console.clear = function() {
+      clear();
+      return proxiedClear.apply(console, arguments);
+    };
+  })();
 
   return {
     writeToConsole: write,
