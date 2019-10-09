85e95f7f9266a449d9ed7e831c8cbca1675a4442
lideo <1573567+lideo@users.noreply.github.com>
2019-10-09 18:01:48 +0800

    Refactor the code that adds custom functions for the console methods

--- src/js/playgroundConsole.js
+++ src/js/playgroundConsole.js
@@ -18,58 +18,48 @@ const createConsole = function() {
     }, '');
   }
 
-  const write = (level = 'log', consoleArguments) => {
-    const node = document.createElement('div');
-    node.className = `console-line ${level}`;
-
-    const message = stringifyArguments([...consoleArguments]);
-
+  function createConsoleLine(level, message) {
     let line = '<p><span>></span>';
     line += (level != 'log') ? `<span class="level">${messageTypeLabels[level]}</span> ` : '';
     line += `${message}</p>`;
 
-    node.innerHTML = line;
-    consoleEl.appendChild(node);
-    scrollToBottom(consoleEl);
+    return line;
+  }
+
+  const write = (level = 'log', consoleArguments) => {
+    if (level == 'clear') {
+
+      clear();
+
+    } else {
+
+      const node = document.createElement('div');
+      node.className = `console-line ${level}`;
+
+      const message = stringifyArguments([...consoleArguments]);
+
+      const line = createConsoleLine(level, message);
+
+      node.innerHTML = line;
+      consoleEl.appendChild(node);
+      scrollToBottom(consoleEl);
+    }
   }
 
   const clear = () => {
     consoleEl.innerHTML = '';
-    write('log', 'Console was cleared.');
   }
 
-  // TODO: rewrite all the rest of the console methods
-  // and maybe refactor...
+  // Rewrite some console methods so we can see the output in our custom console too.
   (function() {
-    const proxiedLog = console.log;
-    console.log = function() {
-      write('log', arguments);
-      return proxiedLog.apply(console, arguments);
-    };
-
-    const proxiedError = console.error;
-    console.error = function() {
-      write('error', arguments);
-      return proxiedError.apply(console, arguments);
-    }
-
-    const proxiedInfo = console.info;
-    console.info = function() {
-      write('info', arguments);
-      return proxiedInfo.apply(console, arguments);
-    };
-
-    const proxiedWarn = console.warn;
-    console.warn = function() {
-      write('warn', arguments);
-      return proxiedWarn.apply(console, arguments);
-    };
-
-    const proxiedClear = console.clear;
-    console.clear = function() {
-      clear();
-      return proxiedClear.apply(console, arguments);
-    };
+    const proxiedMethods = ['log', 'error', 'info', 'warn', 'clear'];
+    proxiedMethods.forEach((method) => {
+      const proxiedMethod = console[method];
+      console[method] = function() {
+        write(method, arguments);
+        return proxiedMethod.apply(console, arguments);
+      }
+    });
   })();
 
   return {
